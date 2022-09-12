bc958072cc8e561cdbe4d4da02f684f70ced3f4f
liyang <liyang@gmail.com>
2022-09-12 19:18:09 +0800

    Gateway优化

---
+++ system-gateway/src/main/java/platform/config/OtherConfig.java
@@ -0,0 +1,24 @@
+package platform.config;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.stream.Collectors;
+
+@Configuration
+public class OtherConfig {
+
+    /**
+     * 通过feign调用微服务报：feign.codec.EncodeException: ...HttpMessageConverters
+     * 下来需要研究一下
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
+        return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
+    }
+}
